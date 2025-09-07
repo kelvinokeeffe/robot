155d85d5dc3c294e41789bf86bdacdb1934d6bc3
kelvinokeeffe <kelvinokeeffe@gmail.com>
2025-09-07 14:27:44 +1000

    Ros2 control added

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -19,14 +19,16 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
+
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
 
     #Inculde the Gazebo Launch File, provided by the gaevo ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
-                )]),
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
     )
 
     # Run the spawner node form the gazebo_ros package. Then entity name doesn't really matter if you only have a single robot
@@ -35,10 +37,25 @@ def generate_launch_description():
                                    '-entity', 'mobile_robot'],
                         output='screen')
 
+    # Run the diff drive controller
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"]
+    )
+
+    # Run the djoint broad controller
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"]
+    )
 
     #Launch all together
     return LaunchDescription([
         rsp,
         gazebo,
-        spawn_entity
+        spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
